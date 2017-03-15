500e315e884f8dbe36c9d17311710a037cab8f6a
Saurav Singh <singh.saurav001@gmail.com>
2017-03-15 12:31:11 +0530

    Added database

--- src/main/java/com/ss/config/CloudConfig.java
+++ src/main/java/com/ss/config/CloudConfig.java
@@ -1,5 +1,7 @@
 package com.ss.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.cloud.app.ApplicationInstanceInfo;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.cloud.config.java.ServiceScan;
@@ -19,6 +21,11 @@ public class CloudConfig extends AbstractCloudConfig {
 	@Bean
 	public ApplicationInstanceInfo applicationInstanceInfo() {
 		return cloud().getApplicationInstanceInfo();
+	}
+	
+	@Bean
+	public DataSource dataSource(){
+		return connectionFactory().dataSource();
 	}
 
 }
--- src/main/java/com/ss/config/LocalConfig.java
+++ src/main/java/com/ss/config/LocalConfig.java
@@ -1,8 +1,13 @@
 package com.ss.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 /**
  * @author saurav
@@ -12,4 +17,9 @@ import org.springframework.context.annotation.Profile;
 @Profile("local")
 public class LocalConfig extends AbstractCloudConfig {
 
+	@Bean
+	public DataSource dataSource() {
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		return builder.setType(EmbeddedDatabaseType.H2).build();
+	}
 }
